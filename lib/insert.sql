f1064bf1378322ef9e8d14b2c7ad53b231a58fa8
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-07 01:26:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,3 +9,5 @@ INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES
 ("dope project", "lazy", 1000000000, "01/01/20", "12/31/20"),
 ("dope project", "lazy", 1000000000, "01/01/20", "12/31/20"),
 ("dope project", "lazy", 1000000000, "01/01/20", "12/31/20");
+
+INSERT INTO users(name, age) VALUES
